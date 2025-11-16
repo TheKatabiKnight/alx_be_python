c0e3b4e3562acb93cc3bb488a88f8a920fd57b9f
TheKatabiKnight <ishakettizoui903@gmail.com>
2025-11-16 17:10:13 +0100

    python calculation code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10;
+number2 = 5;
+addition = number1 +   number2;
+diff = number1 - number2;
+prod = number1 * number2;
+print ("Addition of " + str(number1) + " and " + str(number2) + " is " + str(addition));
+print ("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(diff));
+print ("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(prod));
