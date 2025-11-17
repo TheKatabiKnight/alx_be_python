f7729c54ea8b8eaadf4f0fd7c919945434a01b14
TheKatabiKnight <ishakettizoui903@gmail.com>
2025-11-17 17:35:55 +0100

    adding python file with case by case calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,17 @@
+num1 = float(input('Enter the first number:'))
+num2 = float(input('Enter the second number:'))
+operation = input('Choose the operation (+, -, *, /):')
+match operation:
+    case "+":
+       print('The result is ' + str(num1+num2))
+    case "-":
+       print('The result is ' + str(num1-num2))
+    case "*":
+       print('The result is ' + str(num1*num2))
+    case "/":
+       if num2 > 0:
+          print('The result is ' + str(num1/num2))
+       elif num2 == 0:
+          print('Cannot divide by zero')
+    case _:
+       print('Please enter a valid operation symbol!')
