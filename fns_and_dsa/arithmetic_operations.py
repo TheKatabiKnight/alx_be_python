ea6ce14baa908767600e0fcf306393f273074169
TheKatabiKnight <ishakettizoui903@gmail.com>
2025-12-01 16:55:00 +0100

    new folder for execrices alx

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,10 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            num1 + num2
+        case "subtract":
+            num1 - num2
+        case "multiply":
+            num1 * num2
+        case "divide":
+            num1 / num2
